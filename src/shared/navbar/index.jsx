1379b19df2b52148cec38a0ce6c3f734d59b5a9d
war-turtle <kartikyadav71199@gmail.com>
2019-03-20 21:53:30 +0530

    navbar added

--- src/shared/navbar/index.jsx
+++ src/shared/navbar/index.jsx
@@ -1,26 +1,20 @@
-import React, { Component } from 'react';
-import './navbar.css';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavComponent = () => (
   <div>
-    <nav>
-      <div className="nav-wrapper">
-        <a href="#!" className="brand-logo">Logo</a>
-        <a href="#!" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+    <nav className="transparent z-depth-0">
+      <div className="nav-wrapper container">
+        <Link to="/" className="brand-logo black-text">Inker.ai</Link>
+        <a href="#!" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons black-text">menu</i></a>
         <ul className="right hide-on-med-and-down">
-          <li><a href="sass.html">Sass</a></li>
-          <li><a href="badges.html">Components</a></li>
-          <li><a href="collapsible.html">Javascript</a></li>
-          <li><a href="mobile.html">Mobile</a></li>
+          <li><Link to="/login" className="black-text">Login</Link></li>
         </ul>
       </div>
     </nav>
 
     <ul className="sidenav" id="mobile-demo">
-      <li><a href="sass.html">Sass</a></li>
-      <li><a href="badges.html">Components</a></li>
-      <li><a href="collapsible.html">Javascript</a></li>
-      <li><a href="mobile.html">Mobile</a></li>
+      <li><Link to="/login" className="sidenav-close">Login</Link></li>
     </ul>
   </div>
 );
