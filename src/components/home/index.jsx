112e1e0b11140f5490b6d74eeac3f500f9058898
war-turtle <kartikyadav71199@gmail.com>
2019-03-20 22:00:12 +0530

    home page added

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 const HomeComponent = () => (
   <div>
-    <h1>Inkers.ai</h1>
+    <p className="center-align flow-text">Welcome to Inkers.ai assignment</p>
+    <p className="center-align flow-text">Login to get started</p>
   </div>
 );
 
