17553313d834648120b407920196d2d870595366
war-turtle <kartikyadav71199@gmail.com>
2019-03-20 21:25:59 +0530

    eslint and materializecss added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import NavComponent from './shared/navbar';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
